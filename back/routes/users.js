8229d3159b25874a9fa522829b904e4c3741fc61
haamid10 <apdixamiid919@gmail.com>
2023-06-15 18:15:14 +0300

    routes

---
+++ back/routes/users.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const User = require("../models/user")
+const CryptoJS = require("crypto-js")
+
+
+// Update
+// DELETE
+// GET
+// GET ALL
+// GET USER STATS
