3896e90b74b52ce9d1393d6ea5be1c6f97a5492a
haamid10 <apdixamiid919@gmail.com>
2023-06-18 19:12:14 +0300

    backend ended

--- back/server.js
+++ back/server.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose')
 const dotenv= require('dotenv')
 const authRoute = require('./routes/auth')
 const userRoute = require('./routes/users')
+const movieRoute= require("./routes/movies")
+const listRoute = require("./routes/lists")
 
 dotenv.config()
 mongoose.connect(process.env.MONGO,{
@@ -16,6 +18,8 @@ mongoose.connect(process.env.MONGO,{
 app.use(express.json())
 app.use("/api/auth", authRoute)
 app.use("/api/users", userRoute)
+app.use("/api/movie", movieRoute)
+app.use("/api/list", listRoute)
 app.listen(8080,() => {
     console.log("Server is running on port 8080")
 })
